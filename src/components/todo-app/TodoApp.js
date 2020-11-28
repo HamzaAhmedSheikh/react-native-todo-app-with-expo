1c2ea20f45845abd96ffebddb50156a5d26ba481
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-29 02:31:09 +0500

    styling input box and button.

--- src/components/todo-app/TodoApp.js
+++ src/components/todo-app/TodoApp.js
@@ -4,17 +4,44 @@ import { StyleSheet, Text, TextInput, View, Button, ScrollView, TouchableOpacity
 export default function TodoApp() {
   return (
     <View style={styles.container}>
-      <Text> Hello World </Text>  
+     <Text style={styles.title}> todo </Text>  
+     <View style={styles.inputContainer}>
+      <TextInput 
+        style={styles.textInput}
+        placeholder="Enter Item"
+      /> 
+      <Button title='ADD' />
+     </View>   
     </View>
   )  
 }
 
 
 const styles = StyleSheet.create({
-   container: {
+  container: {
     flex: 1,
     background: '#fff',
     alignItems: 'center',
     paddingTop: 40, 
   }, 
+  title: {
+    fontSize: 64,
+    color: 'lightgrey',
+  }, 
+  inputContainer: {
+    flexDirection: "row",            
+    width: '70%',
+    justifyContent: 'space-between',
+    alignItems: "center",
+  },
+  textInput: {
+    borderColor: "red",
+    // borderWidth: 2,
+    borderBottomWidth: 2,
+    width: '70%',
+    // borderRadius: 50,
+    fontSize: 16,
+    padding: 10,
+  },
+
 });
